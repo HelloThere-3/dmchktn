2922e22b22df55654b20b7f3025899b84fbec734
HelloThere-3 <tabs6lite1723@gmail.com>
2026-04-13 23:31:12 +0530

    Update host.py

--- host.py
+++ host.py
@@ -271,7 +271,7 @@ with st.container():
             Chosen_Model = "Model_Tuned"
         #else:
             #model = model4()
-        #option = st.radio("Choose source:", ("Upload Photo", "Take Photo"),horizontal=True)
+        option = st.radio("Choose source:", ("Upload Photo", "Take Photo"),horizontal=True)
         if option == "Upload Photo":
             uploaded_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
             Uploaded = True
